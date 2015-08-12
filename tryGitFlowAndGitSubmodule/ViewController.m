4f6d22861d1745c90f488a8753816658c15757c0
XiaoYulong <xiaoyulong007@gmail.com>
2015-08-13 00:19:14 +0800

    adjust

--- tryGitFlowAndGitSubmodule/ViewController.m
+++ tryGitFlowAndGitSubmodule/ViewController.m
@@ -16,6 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 30)];
+    label.text = @"哈哈";
+    [self.view addSubview:label];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
