3058804db04832eee6826675759f439ba93a5302
XiaoYulong <xiaoyulong007@gmail.com>
2015-08-13 00:10:55 +0800

    master

--- tryGitFlowAndGitSubmodule/AppDelegate.m
+++ tryGitFlowAndGitSubmodule/AppDelegate.m
@@ -20,6 +20,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor greenColor];
     [self.window makeKeyAndVisible];
+    //master
     return YES;
 }
 
