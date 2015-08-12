f4fa70e85df61d1897e9d09fd2022a5530079047
XiaoYulong <xiaoyulong007@gmail.com>
2015-08-13 00:13:37 +0800

    add root view controller

--- tryGitFlowAndGitSubmodule/AppDelegate.m
+++ tryGitFlowAndGitSubmodule/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -19,6 +20,7 @@
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor greenColor];
+    self.window.rootViewController = [[ViewController alloc] init];
     [self.window makeKeyAndVisible];
     //branch -1
     return YES;
